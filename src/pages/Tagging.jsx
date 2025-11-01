1663f3b67b1e41a0ad55054e57ad1fec44fb528c
uHAL10 <haruto66415@gmail.com>
2025-11-02 02:42:31 +0900

    タグづけ機能完成

--- src/pages/Tagging.jsx
+++ src/pages/Tagging.jsx
@@ -2,14 +2,30 @@ import { useOutletContext, useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
+import './Tagging.css'
+
 function Tagging() {
     const { groupId } = useParams()
     const [tags, setTags] = useState([])
+    const [filteredItems, setFilteredItems] = useState([])
+    const [currentIndex, setCurrentIndex] = useState(0)
     const navigate = useNavigate()
     const { tagGroups } = useOutletContext()
 
     const handleClick = () => {
         navigate(`/`)
+    }
+
+    const handleTagClick = async (tag) => {
+        let currentItem = filteredItems[currentIndex]
+
+        // すでに含まれていないタグであることを確認して、タグを追加
+        if (!currentItem.tags.includes(tag)) {
+            currentItem.tags.push(tag)
+        }
+        await currentItem.save()
+
+        setCurrentIndex(currentIndex + 1)
     }
 
     useEffect(() => {
@@ -37,6 +53,7 @@ function Tagging() {
             console.log('filteredItems:', filteredItems)
     
             setTags(tagsInGroup)
+            setFilteredItems(filteredItems)
         }
         run()
     }, [groupId])
@@ -44,9 +61,19 @@ function Tagging() {
     return (
         <div>
             <button onClick={handleClick}>Home</button>
-            <p>{tags.map((tag, index) => (
-                <div key={index}>{tag}</div>
-            ))}</p>
+
+            {filteredItems.length > 0 && (
+                <div>
+                    <img src={filteredItems[currentIndex].fileURL} className='tagging-image' />
+                    <p>progress: {currentIndex + 1} / {filteredItems.length}</p>
+                </div>
+            )}
+
+            <p>
+                {tags.map((tag, _index) => (
+                    <button onClick={() => handleTagClick(tag)}>{tag}</button>
+                ))}
+            </p>
         </div>
     )
 }
